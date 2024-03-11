ce248516edc1384863088f8e18138b52e77e2edd
Omarmoatz <omar.raouf.002@gmail.com>
2024-03-11 02:17:29 +0200

    add template

--- project/urls.py
+++ project/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from ml.views import predection
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', predection)
 ]
