286ec91f6270f0744e4be6d3abb4b224d795e361
Omarmoatz <omar.raouf.002@gmail.com>
2024-03-11 02:36:44 +0200

    add view

--- ml/views.py
+++ ml/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
+import pandas as pd
 
 from .models import Iris
 from .form import IrisForm
 
 def predection(request):
-    form = IrisForm()
+    if request.method == 'POST':
+        form = IrisForm(request.POST)
+        if form.is_valid():
+            my_form = form.save(commit=False)
+            sepal_length = form.cleaned_data['sepal_length']
+            sepal_width = form.cleaned_data['sepal_width']
+            petal_length = form.cleaned_data['petal_length']
+            petal_width = form.cleaned_data['petal_width']
+
+            model = pd.read_pickle('model')  # Load the trained model
+            result = model.predict([['sepal_length','sepal_width','petal_length','petal_width']])
+
+            predect = result[0]
+            my_form.species = predect
+            my_form.save()
+            return render(request , 'iris.html',{'form':form,'predect':predect})
+    else:
+        form = IrisForm()
     return render(request , 'iris.html',{'form':form})
 
